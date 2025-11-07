9cf6fed339e2f502081ff38c58a91fc770ae3f11
EduardoAndradeSantos <eduardospore@live.com>
2025-11-06 21:03:06 -0300

    feat: projeto final 1 completo

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,6 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.Principal.Principal;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,15 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
-    @Override
-    public void run(String... args) {
-        Principal principal = new Principal();
-        principal.exibeMenu();
-
-    }
-
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.exibeMenu();
+	}
 }
